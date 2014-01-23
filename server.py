a3acd077ad4b8f3c7b104931ac8b109c1a8f2a2e
Jourdan West <westjour@arctic.cse.msu.edu>
2014-01-22 20:28:55 -0500

    my changes1

---
+++ server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import random
+import socket
+import time
+
+s = socket.socket()         # Create a socket object
+host = socket.getfqdn() # Get local machine name
+port = random.randint(8000,9999)
+s.bind((host, port))        # Bind to the port
+
+print 'Starting server on', host, port
+print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+
+s.listen(5)                 # Now wait for client connection.
+
+print 'Entering infinite loop; hit CTRL-C to exit'
+
+while True:
+    print 'inside loop'
+
+    # Establish connection with client.    
+    c, (client_host, client_port) = s.accept()
+    print 'Got connection from'# , client_host, client_port
+    
+    # send a response
+    c.send('HTTP/1.0 200 OK\r\n')
+    c.send('Content-Type: text/html\r\n\r\n')
+    c.send('<html><body>')
+    c.send('<h1>Hello, world</h1>')
+    c.send("this is westjour's Web server.")
+    c.send('</body></html>')
+c.close()
+
+s.close()
